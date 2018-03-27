f53a726df10a74f94c0934a10d9f608a132418c1
shashouzhiwang <1156132456@qq.com>
2018-03-27 23:58:35 +0800

    Signed-off-by: shashouzhiwang <1156132456@qq.com>

--- myapp/src/app/auth/login/login.component.ts
+++ myapp/src/app/auth/login/login.component.ts
@@ -3,6 +3,9 @@ import { Http } from "@angular/http";
 import { HttpService } from "../../provider/http.service";
 import { Config } from "../../provider/config";
 import { HttpEventType, HttpResponse } from "@angular/common/http";
+// import { io } from "socket.io-client";
+import * as io from "socket.io-client";
+
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
@@ -12,6 +15,7 @@ export class LoginComponent implements OnInit {
   history: string ;
   chat: string ;
   ws: any;
+  socket: any;
   constructor(
     public httpService: HttpService,
     public env: Config,
@@ -31,49 +35,21 @@ export class LoginComponent implements OnInit {
         }
       },
       () => {});
+    this.socket = io.connect("http://localhost:3000");
+    this.socket.on("connect", (socket) => {
+      // this.socket.emit("hi!");
+      console.log(this.socket.id);
+      this.socket.on("connect_error", (error) => {
+        console.error(error);
+      });
+      this.socket.on("error", (error) => {
+        console.error(error + "---error");
+      });
+      this.socket.on("disconnect", (error) => {
+        console.error("---disconnect");
+      });
+    });
 
-    this.ws = new WebSocket("ws://localhost:3000/ws/chat");
-
-    this.ws.onmessage = function(event) {
-      let data = event.data;
-      console.log(data);
-      let msg = JSON.parse(data);
-      // if (msg.type === "list") {
-      //   vmUserList.users = msg.data;
-      // } else if (msg.type === 'join') {
-      //   addToUserList(vmUserList.users, msg.user);
-      //   addMessage(vmMessageList.messages, msg);
-      // } else if (msg.type === 'left') {
-      //   removeFromUserList(vmUserList.users, msg.user);
-      //   addMessage(vmMessageList.messages, msg);
-      // } else if (msg.type === 'chat') {
-      //   addMessage(vmMessageList.messages, msg);
-      // }
-    };
-
-    this.ws.onclose = function (evt) {
-      console.log("[closed] " + evt.code);
-      // var input = $('#form-chat').find('input[type=text]');
-      // input.attr('placeholder', 'WebSocket disconnected.');
-      // input.attr('disabled', 'disabled');
-      // $('#form-chat').find('button').attr('disabled', 'disabled');
-    };
-
-    this.ws.onerror = function (code, msg) {
-      console.log("[ERROR] " + code + ": " + msg);
-    };
-
-  //   this.httpService.post(this.env.getEndpoint("products"), {"name": "XBox", "price": 3999}).subscribe(
-  //     (event) => {
-  //       if (event.type === HttpEventType.UploadProgress) {
-  //         // This is an upload progress event. Compute and show the % done:
-  //         const percentDone = Math.round(100 * event["loaded"] / event["total"]);
-  //         console.log(`File is ${percentDone}% uploaded.`);
-  //       } else if (event instanceof HttpResponse) {
-  //         console.log("File is completely uploaded!", event);
-  //       }
-  //     },
-  //     () => {});
   }
 
 
@@ -82,7 +58,8 @@ export class LoginComponent implements OnInit {
     let text = this.chat.trim();
     if (text) {
       this.chat = "";
-      this.ws.send(text);
+      this.socket.emit("test", text);
+      // this.ws.send(text);
     }
   }
 
--- server/app.js
+++ server/app.js
@@ -14,7 +14,18 @@ const rest = require('./rest');
 
 const app = new Koa();
 
-const WebSocketServer = ws.Server;
+var http = require('http').createServer(app.callback());
+
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+    console.log('a user connected'+socket)
+    socket.broadcast.emit('hi');
+    socket.on('test', function(msg){
+        console.error(msg);
+    })
+});
+
 
 app.use(cors());
 
@@ -43,93 +54,10 @@ app.use(rest.restify());
 // add controllers:
 app.use(controller());
 
-let server = app.listen(3000);
+// let server = app.listen(3000);
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
 console.log('app started at port 3000...');
-function createWebSocketServer(server, onConnection, onMessage, onClose, onError) {
-    let wss = new WebSocketServer({
-        server: server
-    });
-    wss.broadcast = function broadcast(data) {
-        wss.clients.forEach(function each(client) {
-            client.send(data);
-        });
-    };
-    onConnection = onConnection || function () {
-        console.log('[WebSocket] connected.');
-    };
-    onMessage = onMessage || function (msg) {
-        console.log('[WebSocket] message received: ' + msg);
-    };
-    onClose = onClose || function (code, message) {
-        console.log(`[WebSocket] closed: ${code} - ${message}`);
-    };
-    onError = onError || function (err) {
-        console.log('[WebSocket] error: ' + err);
-    };
-        let location = {    wss.on('connection', function (ws) {
-            // let location = url.parse(ws.upgradeReq.url, true);
 
-            "href": "/ws/chat",
-            "pathname":"/ws/chat"
-        };
-        console.log('[WebSocketServer] connection: ' + location.href);
-        ws.on('message', onMessage);
-        ws.on('close', onClose);
-        ws.on('error', onError);
-        if (location.pathname !== '/ws/chat') {
-            // close ws:
-            ws.close(4000, 'Invalid URL');
-        }
-        // check user:
-        // let user = parseUser(ws.upgradeReq);
-        let user = {
-            name: "test"
-        };
-        // if (!user) {
-        //     ws.close(4001, 'Invalid user');
-        // }
-        ws.user = user;
-        ws.wss = wss;
-        onConnection.apply(ws);
-    });
-    console.log('WebSocketServer was attached.');
-    return wss;
-}
-var messageIndex = 0;
 
-function createMessage(type, user, data) {
-    messageIndex ++;
-    return JSON.stringify({
-        id: messageIndex,
-        type: type,
-        user: user,
-        data: data
-    });
-}
-
-function onConnect() {
-    let user = this.user;
-    let msg = createMessage('join', user, `${user.name} joined.`);
-    this.wss.broadcast(msg);
-    // build user list:
-    // let users = this.wss.clients.map(function (client) {
-    //     return client.user;
-    // });
-    this.send(createMessage('list', user));
-}
-
-function onMessage(message) {
-    console.log(message);
-    if (message && message.trim()) {
-        let msg = createMessage('chat', this.user, message.trim());
-        this.wss.broadcast(msg);
-    }
-}
-
-function onClose() {
-    let user = this.user;
-    let msg = createMessage('left', user, `${user.name} is left.`);
-    this.wss.broadcast(msg);
-}
-
-app.wss = createWebSocketServer(server, onConnect, onMessage, onClose);
